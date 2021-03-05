bb5b8af239906abe2f942d41ba5f5849887bf5fc
Arriq Singleton <arriqs@gmail.com>
2021-03-05 16:25:59 -0600

    Same issue with the iage import statement being the wrong case, but inside the MightyPaper coomponent

--- components/MightyPaper.js
+++ components/MightyPaper.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import Image from 'next/Image'
+import Image from 'next/image'
 
 const useStyles = makeStyles({
   root: {
